aa0afae64d0ee76f5200f86d138df1bca8bd1822
Yashwanth S <123622523+yashwanths814@users.noreply.github.com>
2026-02-19 18:50:24 +0530

    Format globalForPrisma declaration for readability

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
